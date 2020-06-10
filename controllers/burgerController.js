8f910fd3db5b3f0e8f2c8628cbf42c53015ff92f
Josh R <jriver35@uncc.edu>
2020-06-10 19:19:46 -0400

    Added post route and jquery ajax call, not currently working

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -4,10 +4,18 @@ const burger = require("../models/burger.js")
 
 const router = express.Router()
 
+// Set up routes
 router.get("/", (req, res) => {
     burger.selectAll(data => {
         console.log(data)
         res.render("index", {burgers: data})
+    })
+})
+
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne(burger_name, req.body.burger, result => {
+        // Send back id of new burger
+        res.json({id: result.insertId})
     })
 })
 
---
+++ public/assets/js/index.js
@@ -0,0 +1,19 @@
+$("#createburger").on("submit", function (event) {
+    event.preventDefault();
+
+    // Save user input to variable
+    let newBurger = {
+        burger_name: $("#burgername").val().trim()
+    }
+
+    // Send the post request
+    $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+    })
+    .then(function () {
+        console.log("Created new burger")
+        // Reload page to see updated list
+        location.reload()
+    })
+})

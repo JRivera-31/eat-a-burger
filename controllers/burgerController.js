0e48fdc721403eef6558b577ae48a230bb8628a2
Josh R <jriver35@uncc.edu>
2020-06-10 16:52:09 -0400

    Set up route page

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -4,4 +4,11 @@ const burger = require("../models/burger.js")
 
 const router = express.Router()
 
+router.get("/", (req, res) => {
+    burger.selectAll(data => {
+        console.log(data)
+        res.render("index", {burgers: data})
+    })
+})
+
 module.exports = router
